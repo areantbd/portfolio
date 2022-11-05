06ebe456ab7af6c64f06b73c93e4247c21ed6980
Iván <ivantbd87@gmail.com>
2022-11-05 15:19:36 +0100

    fix: fixed whatsapp contact speed dial

--- portfolio/src/components/UI/speed-dial-component/SpeedDialComponent.jsx
+++ portfolio/src/components/UI/speed-dial-component/SpeedDialComponent.jsx
@@ -8,24 +8,23 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import { grey } from "@mui/material/colors";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { useTheme } from "@mui/material/styles";
 
 
 
 const actions = [
   {
-    icon: <WhatsAppIcon />,
-    name: "+34 693 509 447",
-    onclick: () => {
-      alert("hey");
-    },
+    icon: (
+    <a
+      href="https://wa.me/+34693509447"
+      target="_blank"
+      rel="noreferrer"
+      className="text-success"
+    >
+    <WhatsAppIcon />
+    </a>
+    ),
+    name: "WhatsApp",
+    tag: "contact",
   },
   {
     icon: (
@@ -65,18 +64,6 @@ const greyLight = grey[500];
 const greyDark = grey[800];
 
 export default function SpeedDialComponent() {
-  const [open, setOpen] = React.useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <>
       <Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
@@ -98,33 +85,10 @@ export default function SpeedDialComponent() {
               key={action.name}
               icon={action.icon}
               tooltipTitle={action.name}
-              onClick={handleClickOpen}
             />
           ))}
         </SpeedDial>
       </Box>
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Feel free to contact me on: <br/>
-            WhatsApp: +34 693509 447 <br/>
-            Telegram: https://t.me/Ivan_RF
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
     </>
   );
 }
